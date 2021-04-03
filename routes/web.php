52ef95ccb7894ead5cf0854068c71d125b7129a7
ymershabani1 <ymer.shabani@student.cacttus.education>
2021-04-03 03:56:01 +0200

    Update web.php part 2

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function () {
+Route::get('/bab', function () {
     return view('welcome');
 });
-Route::get('/', function () {
+Route::get('/baba', function () {
     return view('welcome');
 });
-Route::get('/', function () {
+Route::get('/babab', function () {
     return view('welcome');
 });
