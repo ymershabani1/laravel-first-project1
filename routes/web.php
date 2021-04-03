4ca77df9a8d5e95c49657d5c9f465acf1cc46941
ymershabani1 <ymer.shabani@student.cacttus.education>
2021-04-03 03:47:20 +0200

    summary

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-Route::get('/welcome1', function () {
-    return view('welcome');
-});
